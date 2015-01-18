4d7d1b0a33fe3ac08be2b8e39c9865c240a10a35
hefangshi <hefangshi@gmail.com>
2015-01-18 16:54:41 +0000

    [fis-deploy-git] commit 1421600081499

--- components/add-project/add-project.js
+++ components/add-project/add-project.js
@@ -1,7 +1,7 @@
 F.define('add-project', function(Frequire, exports, module){var core = F.require('core');
 
 module.exports = {
-    template: "<c-folder-select v-ref=\"folderSelect\" class=\"group\" v-with=\"height: '430px'\"></c-folder-select>",
+    template: "<c-folder-select v-ref=\"folderSelect\" class=\"group\" v-with=\"height: '390px'\"></c-folder-select>",
     inherit: true,
     components: {
         'c-folder-select': F.require('folder-select')
