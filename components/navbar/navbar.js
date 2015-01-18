40b028dc14b43cef2a70d0ab57eeb0fa8b2c5a15
hefangshi <hefangshi@gmail.com>
2015-01-18 16:35:34 +0000

    [fis-deploy-git] commit 1421598934032

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,5 +1,5 @@
 F.define('navbar', function(Frequire, exports, module){module.exports = {
-    template: "<ul class=\"fg-navbar\">\n    <li v-class=\"active: active==name\" v-on=\"click: onClick(this)\" v-repeat=\"items\">\n        <span class=\"glyphicon {{icon}}\"></span>\n        <span>{{name}}</span>\n    </li>\n</ul>",
+    template: "<ul class=\"fg-navbar\">\n    <li v-class=\"active: currentView==view\" v-on=\"click: onClick(this)\" v-repeat=\"items\">\n        <span class=\"glyphicon {{icon}}\"></span>\n        <span>{{name}}</span>\n    </li>\n</ul>",
     inherit: true,
     data: function(){
         return {
@@ -11,12 +11,12 @@ F.define('navbar', function(Frequire, exports, module){module.exports = {
                     "view": "c-index",
                 },
                 {
-                    "name": "Project",
+                    "name": "Projects",
                     "icon": "glyphicon-folder-open",
                     "view": "c-projects"
                 },
                 {
-                    "name": "Setting",
+                    "name": "Settings",
                     "icon": "glyphicon-cog",
                     "view": "c-setting"
                 }
