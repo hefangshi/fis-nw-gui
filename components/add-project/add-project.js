a8a01a296cb6da5f60a3efb67aa27f3141c8983d
hefangshi <hefangshi@gmail.com>
2015-01-18 14:58:45 +0000

    [fis-deploy-git] commit 1421593124928

--- components/add-project/add-project.js
+++ components/add-project/add-project.js
@@ -1,7 +1,7 @@
 F.define('add-project', function(Frequire, exports, module){var core = F.require('core');
 
 module.exports = {
-    template: "<span class=\"title light\">Add Project</span>\n<c-folder-select v-ref=\"folderSelect\" class=\"group\"></c-folder-select>\n<!-- <div class=\"panel-buttons\">\n    <button type=\"button\" class=\"btn btn-default\" v-on=\"click: this.currentView='c-index'\">Cancel</button>\n    <button type=\"button\" class=\"btn btn-primary\" v-on=\"click: onOK\">OK</button>\n</div> -->",
+    template: "<c-folder-select v-ref=\"folderSelect\" class=\"group\" v-with=\"height: '430px'\"></c-folder-select>",
     inherit: true,
     components: {
         'c-folder-select': F.require('folder-select')
